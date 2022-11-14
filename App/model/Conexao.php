03a6efbce40644525d999748c5f7960602b56fab
dorowel <dorowel@gmail.com>
2022-11-14 12:29:41 -0300

    update 14112022

---
+++ App/model/Conexao.php
@@ -0,0 +1,17 @@
+<?php
+
+class Conexao {
+
+    private static $instance;
+
+    public static function getConn()
+    {
+        if(!isset(self::$instance)):
+
+            self::$instance = new PDO('mysql:host=localhost;dbname=pdo;charset=utf8','root','');
+            else:
+            return self::$instance;
+            endif;
+    }
+
+}
